287592f2e3ea955575802cdfe37ac86b095d75bd
proshanto-c <proshanto.chanda@gmail.com>
2026-09-25 21:56:11 +0100

    chore(release): bump version to 0.1.1

--- imagetochaste/__init__.py
+++ imagetochaste/__init__.py
@@ -3,7 +3,7 @@ ImageToChaste: Turn microscopy images into Chaste C++ simulation meshes
 using Meta's Segment Anything Model 2 (SAM 2).
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from imagetochaste.exporters.base import ChasteMesh, Element, Node
 from imagetochaste.exporters.chaste_formatter import (
